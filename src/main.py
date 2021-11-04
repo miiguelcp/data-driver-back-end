f3c4c96f7b5ea93f0ac62c1f70f33eebad9a0b2d
lbideau <leshliebzt@gmail.com>
2021-11-04 13:34:04 -0400

    New Commit 13:33

--- src/main.py
+++ src/main.py
@@ -89,5 +89,5 @@ def user_login():
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
+    PORT = int(os.environ.get('PORT', 3500))
     app.run(host='0.0.0.0', port=PORT, debug=False)
